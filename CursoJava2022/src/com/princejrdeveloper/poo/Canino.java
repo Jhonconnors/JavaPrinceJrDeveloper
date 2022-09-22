26b1ec25128315c7f7d3281147fde673ddbd0528
Jhon K. Peña R <72120063+Jhonconnors@users.noreply.github.com>
2022-09-22 01:12:16 -0300

    Delete CursoJava2022 directory
    Remplace

--- CursoJava2022/src/com/princejrdeveloper/poo/Canino.java
+++
@@ -1,7 +0,0 @@
-package com.princejrdeveloper.poo;
-
-public interface Canino {
-
-	 public abstract void aullar();
-     public abstract void ladrar();
-}
--- CursoJava2022/src/com/princejrdeveloper/poo/Instrumento.java
+++
@@ -1,7 +0,0 @@
-package com.princejrdeveloper.poo;
-
-public abstract class Instrumento {
-
-	String tipo;
-	public abstract String tocar();
-}
