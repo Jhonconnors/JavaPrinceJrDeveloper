6d4ac6b5afc14d2d301afc9bd833d3e75b71997f
Jhon K. Peña R <72120063+Jhonconnors@users.noreply.github.com>
2022-09-22 01:03:07 -0300

    Delete CursoJava2022/src/com/princejrdeveloper/test directory

--- CursoJava2022/src/com/princejrdeveloper/test/Jubilacion.java
+++
@@ -1,8 +0,0 @@
-package com.princejrdeveloper.test;
-
-public class Jubilacion {
-	
-	  public static  double sFinal(double salario) {
-	    return salario*0.70;
-	  }
-	}
--- CursoJava2022/src/com/princejrdeveloper/test/Operacion.java
+++
@@ -1,6 +0,0 @@
-package com.princejrdeveloper.test;
-
-public interface Operacion {
-
-	double calcular(double num1, double num2);
-}
